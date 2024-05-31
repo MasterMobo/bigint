b13e99a0199e9cf9d7939ab0b8a6a57c6ddd284a
Bui Dang Khoa <mastermobo101@gmail.com>
2024-05-31 12:40:43 +0700

    removed build files

--- build/src/CMakeFiles/bigint.dir/bigint/utils/Node.cpp.o.d
+++
@@ -1,5 +0,0 @@
-src/CMakeFiles/bigint.dir/bigint/utils/Node.cpp.o: \
- /home/khoa/programming/bigint/src/bigint/utils/Node.cpp \
- /usr/include/stdc-predef.h \
- /home/khoa/programming/bigint/src/../include/bigint/utils/Node.h \
- /home/khoa/programming/bigint/src/../include/bigint/utils/DLList.h
